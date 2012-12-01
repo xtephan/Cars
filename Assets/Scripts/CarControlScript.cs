b9c385674ddb83f85f14619b0c499d67065d3d69
Stefan Fodor <stefan@otinsoft.com>
2012-12-01 23:08:53 +0100

    Wheel Spin

--- Assets/Scripts/CarControlScript.cs
+++ Assets/Scripts/CarControlScript.cs
@@ -7,6 +7,8 @@ public class CarControlScript : MonoBehaviour {
 	//Wheels
 	public WheelCollider wheelFL, wheelFR, wheelRL, wheelRR; 
 	
+	public Transform wheelFLTrans, wheelFRTrans, wheelRLTrans,wheelRRTrans;
+	
 	float maxTorque = 50f;
 	
 	// Use this for initialization
@@ -28,5 +30,14 @@ public class CarControlScript : MonoBehaviour {
 		// Apply Steering
 		wheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
 		wheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+	}
+	
+	void Update() {
+	
+		wheelFLTrans.Rotate(wheelFL.rpm/60*360*Time.deltaTime, 0, 0);
+		wheelFRTrans.Rotate(wheelFR.rpm/60*360*Time.deltaTime, 0, 0);
+		wheelRLTrans.Rotate(wheelRL.rpm/60*360*Time.deltaTime, 0, 0);
+		wheelRRTrans.Rotate(wheelRR.rpm/60*360*Time.deltaTime, 0, 0);
+		
 	}
 }
